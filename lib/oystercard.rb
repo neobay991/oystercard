af40199a48ed9a28f3e577e1fe6143ab43081802
Elishka Flint <elishkaf@gmail.com>
2018-06-18 15:03:21 +0100

    Added test for initial balance set to zero

---
+++ lib/oystercard.rb
@@ -0,0 +1,11 @@
+class OysterCard
+
+  attr_reader :balance
+
+  
+
+  def initialize
+    @balance = 0
+  end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,13 @@
 require 'oystercard'
 
 describe OysterCard do
-  describe 'check balance is 0 at the start' do
-    expect(subject.balance).to eq 0
+
+  describe '#initialize' do
+
+    it 'sets balance to 0' do
+      expect(subject.balance).to eq 0
+    end
+
   end
 
 end
